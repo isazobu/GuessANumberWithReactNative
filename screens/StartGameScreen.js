24b498f52e322ded9110ef81aaa051a46d885147
isa <isaazobu@gmail.com>
2020-09-08 13:50:59 +0300

    Added TextInput and Buttons

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,10 +1,18 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native'
 
 const StartGameScreen = () => {
     return (
         <View style={styles.gameScreen}>
-            <Text>The Game Screen</Text>
+            <Text style={styles.title}>Start a New Game</Text>
+            <View style={styles.inputContainer}>
+                <Text>Select a Number</Text>
+                <TextInput  />
+            </View>
+            <View style={styles.btnContainer}>
+                <Button title="Reset" />
+                <Button title="Confirm" />
+            </View>
         </View>
     )
 }
@@ -16,7 +24,22 @@ const styles = StyleSheet.create({
         flex:1,
         paddingTop: 20,
         alignItems:'center',
-        justifyContent:'center'
-       
+    },
+    title: {
+        fontSize:20,
+        marginVertical:10
+    },
+    inputContainer: {
+        width:300,
+        maxWidth:'80%',
+        alignItems:'center'
+    },
+    btnContainer: {
+        flexDirection:'row',
+        width:'100%',
+        justifyContent:'space-around',
+        
+
     }
+
 })
